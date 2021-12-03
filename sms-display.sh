8935d5cb11bd4ed98d59d1c79627ff387dd8853e
m00E10 <93796498+m00E10@users.noreply.github.com>
2021-12-02 20:16:49 -0500

    Update sms-display.sh

--- sms-display.sh
+++ sms-display.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 number=$1
-echo "Positional arg = $number"
 
 function number_check {
   if [ -z $number ]; then
@@ -11,22 +10,19 @@ function number_check {
 }
 
 function prettify {
-  smslog=sample-sms.log # ~/.local/share/sxmo/$number/sms.txt
-  cat $smslog | sed 's/from//g' | sed 's/to/    /g' | sed 's/+\w\+//' \
-   sed 's/at//g' | sed 's/T/ /g' | sed 's/[[:digit:].]*\:$//' | \
-   sed 's/[[:digit:].]*\:$//' | sed 's/-$//' | sed 's/-$//' | sed 's/+$//' \
-   > /tmp/$number
+  smslog=~/.local/share/sxmo/modem/$number/sms.txt
+  watch -n 5 "cat $smslog | sed -E '/^(Received|Sent) (from|to) \+[0-9]+ at/ s/ .*([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9:]{8}).*/        \1 \2/' | tee /tmp/$number" &>/dev/null &
 }
 
 function make_panes {
   SESSION=sms
-  # $1 = rows $2 = columns
-  set -- $(stty size) 
-  # $1 - 1 because tmux status line uses a row
-  tmux -2 new-session -d -s "$SESSION" -x "$2" -y "$(($1 - 1))" \
-   "exec less +F $smslog"
+  set -- $(stty size) # $1 = rows $2 = columns
+  tmux -2 new-session -d -s "$SESSION" -x "$2" -y "$(($1 - 1))" "exec less +F /tmp/$number" # $1 - 1 because tmux status line uses a row
   tmux select-window -t $SESSION:0
-  #tmux split-window -h "exec bottom-pane.sh $number"
-  tmux split-window -v -l 5 -t 0 'exec echo "hello"'
+  tmux split-window -v -l 5 -t 0 "exec while true; do read input; echo $input > /tmp/text; cat /tmp/text | sxmo_modemsendsms.sh $number -; done"
   tmux -2 attach-session -t $SESSION
 }
+
+number_check
+prettify
+make_panes
